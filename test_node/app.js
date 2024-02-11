bde8d03c37d369781ba2a6f8a68bd70d900512b1
saif <saifeddinebenna@gmail.com>
2024-02-11 16:36:08 +0000

    11/02

--- test_node/app.js
+++ test_node/app.js
@@ -2,7 +2,7 @@ require('express')
 
 app= require('express')
 app.use(express.json())
-
+app.use()
 const port = 3000;
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);
