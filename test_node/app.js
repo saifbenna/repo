9fe211990d55a30ec4b8a7f2be0f20b5d921d83e
saifbenna <41490539+saifbenna@users.noreply.github.com>
2024-02-11 17:33:53 +0100

    Update app.js

--- test_node/app.js
+++ test_node/app.js
@@ -1,5 +1,8 @@
 require('express')
+
 app= require('express')
+app.use(express.json())
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);
